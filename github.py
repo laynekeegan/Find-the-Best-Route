be1324a5e0400512928d68aa433060db250132a0
laynekeegan <keeganl@my.erau.edu>
2020-10-26 18:59:16 -0700

    initial commit

---
+++ github.py
@@ -0,0 +1,15 @@
+import random
+
+N = 5
+loft = [(random.randint(0, 10), random.randint(0, 10) for x in range(N))]
+for item in loft:
+    print(item)
+
+
+def dist(t):
+    return t[0] ** 2 + t[1] ** 2
+
+
+w = list(map(dist, loft))
+
+print(w)
